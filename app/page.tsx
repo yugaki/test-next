fa2605120e26100a88a4d7a31600a85bba9bc63c
y_yoshida <y_yoshida@fieldweb.co.jp>
2024-04-17 16:48:23 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 
+import styles from "./top.module.scss"
 export default function Home() {
   return (
-    <p>hellow yugaki</p>
+
+    <>
+      <p className="bg-blue-500">hellow yugaki</p>
+      <h1>タイトルタイトル</h1>
+    </>
   );
 }
